f2ecf1429f68ab9f39aee7774d55357efd2cf1c1
Abrar Rauf <abrar.rauf338@gmail.com>
2025-07-10 17:25:07 -0400

    automate exec_tern_single to readin cached L_params

--- scripts/fit_binaries.py
+++ scripts/fit_binaries.py
@@ -4,12 +4,20 @@ from gliquid.config import data_dir
 import gliquid.load_binary_data as lbd
 from pymatgen.analysis.phase_diagram import PDPlotter
 import matplotlib.pyplot as plt
+import json
+
 
 def main():
+    dump_dir = "all_dumps/binary_fits"
     os.environ["NEW_MP_API_KEY"] = "Rtb4ppAs9rcNVzh10IVdBRh6HwlBymcJ"
-    param_format = 'linear'   
+    # param_format = 'linear'   
+    param_format = 'whs'   
 
-    binary_sys_name = 'Te-Zr'
+    binary = ['Zr', 'Ga']
+    
+    binary_sys_name = ("-").join(sorted(binary))
+
+    print(binary_sys_name)
 
     binary_json, binary_component_data, binary_digitized_liquidus = lbd.load_mpds_data(binary_sys_name, verbose=True)
 
@@ -33,15 +41,18 @@ def main():
     plt.show()
 
     fit_results = binary_system.fit_parameters(verbose=True, n_opts=3)[0]
-
-    for field, value in fit_results.items():
-        if isinstance(value, float):
-            print(f'{field.upper()}: {value:.3f}')
+    keys_to_extract = ['mae', 'rmse', 'norm_mae', 'norm_rmse', 'L0_a', 'L0_b', 'L1_a', 'L1_b', 'L0', 'L1']
+    dump_dict = {k: fit_results[k] for k in keys_to_extract if k in fit_results}
+    print(dump_dict)
 
     binary_plotter.show('fit+liq')
     binary_plotter.show('nmp')
     plt.show()
 
+    os.makedirs(dump_dir, exist_ok=True)
+    dump_path = os.path.join(dump_dir, f"{binary_sys_name}_{param_format}_fit_results.json")
+    with open(dump_path, "w") as f:
+        json.dump(dump_dict, f, indent=4)
 
 if __name__ == "__main__":
     main()
--- scripts/ternary_interpolation/exec_tern_single.py
+++ scripts/ternary_interpolation/exec_tern_single.py
@@ -2,24 +2,54 @@ from ternary_HSX import ternary_gtx_plotter
 import plotly.offline as ploff
 from gliquid.config import data_dir
 import os
+import json
 
-dump_dir = "all_dumps/"
+dump_dir = "all_dumps/ternary_htmls/"
+read_dir = "all_dumps/binary_fits/"
 
-def plot_BiCdSn_system():
+def plot_ternary_system():
     # Bi-Cd-Sn system
     os.environ["NEW_MP_API_KEY"] = "Rtb4ppAs9rcNVzh10IVdBRh6HwlBymcJ"
-    tern_sys = ["Bi", "Cd", "Sn"]
-    binary_L_dict = {"Bi-Cd": [-685, 4.41, 837, -2.96],
-                     "Cd-Sn": [10649, -10.13, -7219, 15.72],
-                     "Sn-Bi": [-1579, 8.06, 2295, -2.1]}
+    tern_sys = ["Ga", "Zr", "Te"]
+    param_format = 'whs'
+    # param_format = 'linear'
+
+    sorted_sys = sorted(tern_sys)
+    binary_sys_labels = [
+        f"{sorted_sys[0]}-{sorted_sys[1]}",
+        f"{sorted_sys[1]}-{sorted_sys[2]}",
+        f"{sorted_sys[2]}-{sorted_sys[0]}"
+    ]
+
+    print(binary_sys_labels)
+
+    binary_L_dict = {}
+
+    for sys in binary_sys_labels:
+        flipped_sys = "-".join(sorted(sys.split("-")))
+        print(flipped_sys)
+        json_path = os.path.join(read_dir, f"{sys}_{param_format}_fit_results.json")
+        if not os.path.exists(json_path):
+            json_path = os.path.join(read_dir, f"{flipped_sys}_{param_format}_fit_results.json")
+            
+        with open(json_path, "r") as f:
+            params = json.load(f)
+        binary_L_dict[sys] = [
+            params["L0_a"],
+            params["L0_b"],
+            params["L1_a"],
+            params["L1_b"]
+        ]
+
+    print(binary_L_dict)
     
     plotter = ternary_gtx_plotter(tern_sys, data_dir, interp_type="linear", param_format="linear",
                                   L_dict=binary_L_dict, temp_slider=[0, -300], T_incr=10, delta=0.025)
     plotter.interpolate()
     plotter.process_data()
     tern_fig = plotter.plot_ternary()
-    ploff.plot(tern_fig, filename=dump_dir + 'BiCdSn_system.html', auto_open=True)
+    ploff.plot(tern_fig, filename=dump_dir + f'{"-".join(sorted_sys)}_system.html', auto_open=True)
 
 
 if __name__ == "__main__":
-    plot_BiCdSn_system()
+    plot_ternary_system()
